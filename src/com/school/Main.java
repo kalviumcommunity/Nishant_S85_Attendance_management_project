3666d147dba1efb6993054ff70eca4f858b13701
Nishant <rd5762331@g,ail.com>
2025-07-14 09:38:33 +0530

    chore: updated README and fixed Main output (Part 2)

--- src/com/school/Main.java
+++ src/com/school/Main.java
@@ -2,7 +2,24 @@ package com.school;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to the School Attendance System Project!");
-        System.out.println("Session 1: Project Setup and Orientation Complete.");
-}
+        Student[] students = {
+            new Student("Madhav", 24111386),
+            new Student("Nishant", 22111387),
+        };
+
+        Course[] courses = {
+            new Course("DSA", "M11"),
+            new Course("OOPS", "S01")
+        };
+
+        System.out.println("Students Details:");
+        for (Student s : students) {
+            s.display();
+        }
+
+        System.out.println("\nCourses Details:");
+        for (Course c : courses) {
+            c.display();
+        }
+    }
 }
